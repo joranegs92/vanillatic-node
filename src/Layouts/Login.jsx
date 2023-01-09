2e68c4203428c34d159d2a7200a50f94592a0f32
joranegs92 <joranegs92@gmail.com>
2023-01-09 22:55:51 +0900

    프록시설정, 로그인 기능

--- src/Layouts/Login.jsx
+++ src/Layouts/Login.jsx
@@ -1,6 +1,22 @@
-import React, {useCallback, useContext} from 'react';
+import React, {useCallback, useContext, useRef, useState} from 'react';
+import axios from "axios";
+
 function Login({}) {
 
+	const emailRef = useRef();
+	const passwdRef = useRef();
+	const login = () =>{
+			axios.post(
+				'http://localhost:8080/login',
+				JSON.stringify({
+					email : emailRef.current.value,
+					password: passwdRef.current.value
+				}),
+				{headers: {'Content-Type': "application/json"}}).then(({data})=>{
+					alert("로그인되었습니다.")
+				}).catch(e => console.error(e))
+
+	}
 	return(
 		<div id="app" class="container">
 			<section class="login-form">
@@ -9,17 +25,17 @@ function Login({}) {
 				<h1>WELCOME TO VANILLATIC</h1>
 				<form action="#"/>
 					<div class="int-area">
-						<input type="text" id="userId" autocomplete="off"/> >
+						<input type="text" id="userId" autocomplete="off" ref={emailRef}/> >
 							<label for="userId">E-mail</label>
 
 					</div>
 
 					<div class="int-area">
-						<input type="password" name="pw"/>
+						<input type="password" name="pw" ref={passwdRef}/>
 							<label for="pw">Password</label>
 					</div>
 					<div class="btn-area">
-						<button>LOGIN</button>
+						<button onClick={login}>LOGIN</button>
 					</div>
 
 				</section>
